2388e605c7385f284f605807802b2a37f8c5ee57
GodeGeek <arno.clara2016@gmail.com>
2020-12-16 16:34:05 +0200

    added login/register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
+//importing all needed modules
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../app/home/home.component';
 import { FinanceComponent } from './finance/finance.component';
+import { TaxComponent } from '../app/tax/tax.component';
+import { PeopleComponent } from '../app/people/people.component';
+import { ContactComponent } from '../app/contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+
+//add paths to each component
 const routes: Routes = [
-  {path : '', component: HomeComponent},
-  {path : 'finance', component: FinanceComponent}
+  {path : '', component : HomeComponent},
+  {path : 'finance', component : FinanceComponent},
+  {path : 'tax', component : TaxComponent},
+  {path : 'people', component : PeopleComponent},
+  {path : 'contact', component : ContactComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'register', component : RegisterComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FinanceComponent } from './finance/finance.component';
+import { TaxComponent } from './tax/tax.component';
+import { PeopleComponent } from './people/people.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FinanceComponent
+    FinanceComponent,
+    TaxComponent,
+    PeopleComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
